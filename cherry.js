43e4833f5498b83af276e472ac35aaa2c4ed3d33
Judson Silva <judsonbarroso@gmail.com>
2014-01-27 08:05:34 -0200

    Change templates, add dataTypes

--- cherry.js
+++ cherry.js
@@ -1,8 +1,34 @@
+(function(){
+
 String.prototype.capitalize = function(){
 	return this.replace(/^./, function(letter){
 		return letter.toUpperCase();
 	});
 }
+/* Captalize the first letter */
+function capitalize( str ){
+	return str.replace(/^./, function(letter){
+		return letter.toUpperCase();
+	});
+};
+/* Convert to Array */
+function toArray(obj){
+	return Array.prototype.slice.call(obj, 0);
+};
+/* Recursive extend */
+function extend(){
+	var extended = {};
+	$.each( toArray( arguments ), function( i, obj ){
+		$.each( obj, function( key, val ){
+			extended[key] = val.constructor == Object ? 
+				( extended.hasOwnProperty(key) ?
+					extend( extended[key], val ):val
+				) : val;
+		});
+	});
+	return extended;
+}
+
 App = {
 	options: {
 		engine: 'cakephp',
@@ -32,6 +58,44 @@ App = {
 	},
 	setup: function( options ){
 		this.options = $.extend(this.options, options);
+	},
+	templates: {
+		form:"<form action='#' data-resource='{%= o.resource %}' class='cherry-form {%= o.resource %}'><fieldset><legend>{%= o.title %}</legend>{% $.each(o.fields, function(name, attr){ %}{% include(App.templates.field, attr); }); %}<div class='form-actions'><button class='btn submit-cherry-form'>{%= o.submit %}</button></div></fieldset></form>",
+		field:"{% var output = []; o.class = o.class ? o.class + ' cherry-input' : 'cherry-input'; $.each(o, function( attr, value ){ if( attr == 'title'){ o.class += ' cherry-tooltip' } else if ( attr == 'class' || attr == 'label' ){ return true } output.push( attr + '=' + value ); }); %}<div class='cherry-field cherry-{%= o.type %}'>{% if( o.type == 'checkbox' ){ %}<label class='cherry-label {%= o.type%}'><input {%= output.join(' ') %} class='{%= o.class %}'>{%= o.label %}</label>{% } else { %}<label class='cherry-label'>{%= o.label %}</label><input {%= output.join(' ') %} class='{%= o.class %}'>{% } %}</div>"
+	},
+	dataTypes: {
+		string:{
+			constructor: String,
+			field: 'text'
+		},
+		number:{
+			constructor: Number,
+			field:'number'
+		},
+		boolean:{
+			constructor: Boolean,
+			field:'checkbox'
+		},
+		date:{
+			constructor: Date,
+			field:'date'
+		},
+		email:{
+			constructor: String,
+			field:'email'
+		},
+		list:{
+			constructor: Number,
+			field:'radio'
+		},
+		option:{
+			constructor: Number,
+			field:'select'
+		}
+		text:{
+			constructor: String,
+			field:'textarea'
+		}
 	}
 };
 
@@ -40,13 +104,13 @@ App.base.prototype = {
 	constructor: App.base,
 	validation: null,
 	checkator: [],
-	find: function(){
+	find: function( callback ){
 		App.request( this.plural + '/list', { limit:5 } , function(){
 
 		});
-	},	
+	},
 	setup: function( validation ){
-		this.validator = validation;
+		this.validation = validation;
 		return this;
 	},
 	_validate: function(){
@@ -63,33 +127,6 @@ App.base.prototype = {
 		});
 		return isValid;
 	},
-	makeForm: function( fields ){
-		var fields = this.validation ? $.extend(validation, fields) : fields,
-			form = $('<form action="#" onsubmit="#"></form>');
-
-		$.each( fields, function( name, value ){
-			var defaults = {
-				label: name,
-				type: 'text',
-				required: 'required'	
-			};
-			if( value.constructor == Function ){
-				if( value == String ){ defaults.type == 'text' }
-				else { defaults.type = value.name.toLowerCase() }
-				value = defaults;
-			} else { value = $.extend(defaults, value) }
-
-			form.append(
-				'<div class="input"><label>'+ value.label +
-				'</label><input name="'+name +
-				'" type="'+ value.type +
-				'" required="required"></div>'
-			);
-		});
-		form.append('<button type="submit">UHU</button>');
-
-		return form;
-	},
 	getAll: function(){
 		return this.data || {};
 	},
@@ -101,14 +138,14 @@ App.base.prototype = {
 			key = object; object = {};
 			object[key] = value;
 		}
-		object = $.extend( this.getAll() , object);
+		object = extend( this.getAll() , object);
 		this._set('data', object); 
 		return this;
 	},
 	_toDataFormat: function( key, attr ){
 		var str = '['+ key +']';
 		if( App.options.engine == 'cakephp' )
-			str = 'data[' + this.singular.capitalize() +']' + str ;
+			str = 'data[' + capitalize(this.singular) +']' + str ;
 		else
 			str = this.singular + str;
 
@@ -116,37 +153,42 @@ App.base.prototype = {
 
 		return str;
 	},
-	_makeFields: function(){
+	_makeField: function( name, type ){
 		var defaults = {
 				'Number':{ type:'number' },
 				'String':{ type:'text' },
-				'Date':{ type:'date' }			
+				'Date':{ type:'date' },
+				'Boolean':{ type: 'checkbox' }
 			},
+			field = defaults[ type.name ];
+		field.label = field.name = name;
+		return field;
+	},
+	_makeFields: function(){
+		var self = this,
 			fields = {};
-		
-		$.each( this.validator, function(name, type){
-			var attr = defaults[ type.name ];
-				attr.label = name;
-			fields[name] = attr;
+		$.each( this.validation, function(name, type){
+			fields[name] = self._makeField( name, type );
 		});
-		
 		return fields;
 	},
 	makeForm: function( data ){
-
-		var fields = data.fields ? $.extend(this._makeFields(), data.fields ) : this._makeFields();
-		delete data.fields;
 		
-		var data = $.extend({
+		var data = extend({
 			title: 'Formulário',
 			resource: this.singular,
-			fields: fields
+			fields: this._makeFields(),
+			submit: 'Submit'
 		}, data );
 
-		console.log(data);
-		console.log('KKKk...')
-
-		return tmpl('cherry-form', data);
+		return tmpl( App.templates.form , data);
+	},
+	makeField: function( name, props ){
+		var field = {};
+		if( this.validation.hasOwnProperty( name ) ){
+			field = this._makeField( name, this.validation[name]);
+		}
+		return tmpl(App.templates.field, extend( field, props || {} )); 
 	},
 	serialize: function(){
 		var data = this.getAll(),
@@ -188,6 +230,31 @@ App.base.prototype = {
 		} else {
 			callback({ error: 'Erro na validação dos dados' });
 		}
-	}
+	},
+	getDataForm:function( form ){
+		var convert = this.validation,
+			values = [];
+			data = {};
 
+		form.find('textarea, input, select').each(function(){
+			console.log(this, this.constructor);
+		});
+		
+		$.each( form.serializeArray(), function( i, field ){
+			data[ field.name ] = convert[ field.name]( field.value );
+		});
+
+		return data;
+	},
+	onSubmit: function( callback ){
+		var form = $('.cherry-form[data-resource="'+ this.singular +'"]'),
+			self = this;
+
+		form.on('click','.submit-cherry-form', function(ev){
+			ev.preventDefault();
+			callback.call(form, self.getDataForm(form) );
+		});
+	}
 }
+
+})();
